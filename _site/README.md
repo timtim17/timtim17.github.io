2d12d9be7383d938b803aceee5b606eb31235348
Austin Jenchi <ajenchi9@gmail.com>
2019-12-22 19:25:07 -0800

    start converting to jekyll

---
+++ _site/README.md
@@ -0,0 +1,17 @@
+[timtim17.github.io](https://timtim17.github.io)
+==================
+
+website work in progress... yeah...
+
+```html
+<website>
+    <progress style="display: none;" />
+    <script>
+        try {
+            document.querySelector("progress").load();
+        } catch (err) {
+            procrastinate();
+        }
+    </script>
+</website>
+```
---
+++ _site/scripts/projects.js
@@ -0,0 +1,19 @@
+"use strict";
+
+(function() {
+    window.addEventListener("load", init);
+
+    const id = id => document.getElementById(id);
+    const qsa = selector => document.querySelectorAll(selector);
+
+    function init() {
+        qsa("main > .card > button").forEach(btn => btn.addEventListener("click", overlayAnimation));
+    }
+
+    function overlayAnimation() {
+        id("overlay").classList.add("visible");
+        setTimeout(() => {
+            location.href = "project.html";
+        }, 1100)
+    }
+})();
